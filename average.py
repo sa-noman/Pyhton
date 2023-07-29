2e80518460a4f839fe46d75183d6cad8b5ef0796
Shah Abdullah Al Noman <sanoman.bd@gmail.com>
2023-07-29 10:20:28 +0600

    Create average.py
    calculates the average of 4 numbers

---
+++ average.py
@@ -0,0 +1,9 @@
+numbers = [0, 1, 2, 3]
+
+numbers[0] = int(input("Please enter a number: "))
+numbers[1] = int(input("Please enter a number: "))
+numbers[2] = int(input("Please enter a number: "))
+numbers[3] = int(input("Please enter a number: "))
+
+average = (numbers[0] + numbers[1] + numbers[2] + numbers[3]) / 4
+print("Average Value is =", average)
